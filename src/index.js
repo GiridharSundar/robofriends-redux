66ffea4967a482e4c8991787ba63ef535e1f448a
GiridharSundar <giridharsundar8@gmail.com>
2020-11-08 23:39:28 +0400

    adding thunk Middleware

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {createLogger,applyMiddleware} from 'redux-logger'; 
-import {createStore} from 'redux';
+import {createStore ,applyMiddleware} from 'redux';
+import thunkMiddleware from 'redux-thunk'
+import {createLogger} from 'redux-logger'; 
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import {searchRobots} from './reducer'
@@ -10,7 +11,7 @@ import './index.css';
 import 'tachyons'; 
 
 const logger = createLogger();
-const store = createStore(searchRobots,applyMiddleware(logger));
+const store = createStore(searchRobots,applyMiddleware(thunkMiddleware, logger));
 
 ReactDOM.render(
 	<Provider store = {store}>
